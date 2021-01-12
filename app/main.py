bb3d877c2992d020729f158f6d59014762e8d19e
David Lo <davidlo.net@gmail.com>
2021-01-12 15:48:45 +0800

    refactor function names

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
-import shutil
 import pathlib
+import shutil
 from uuid import uuid4
+
+from PIL import Image
 from fastapi import FastAPI, File, UploadFile, BackgroundTasks, HTTPException
 from fastapi.responses import FileResponse
-from PIL import Image
 
 # Server constants
 SERVER_HOST = 'http://127.0.0.1:8000'
@@ -18,26 +19,27 @@ COMPRESS_FOLDER = 'uploads/compress/'
 # Init FastAPI
 app = FastAPI()
 
+
 @app.post('/image')
 async def post_image(image: UploadFile = File(...), background_tasks: BackgroundTasks = None):
     # early return if file type is not valid
     if pathlib.Path(image.filename).suffix.lower() not in FILE_FILTER:
-        return {'error': 'use %s formats only' % format(','.join(FILE_FILTER))}
+        return {'error': f'use {format(",".join(FILE_FILTER))} formats only'}
 
     # create folder if not exists
     if not os.path.exists(ORIGINAL_FOLDER):
         os.makedirs(ORIGINAL_FOLDER)
 
     # target folder + generated file name + file extensions
-    target_file = '{0}{1}{2}'.format(ORIGINAL_FOLDER, str(uuid4()), pathlib.Path(image.filename).suffix)
+    target_file = f'{ORIGINAL_FOLDER}{uuid4()}{pathlib.Path(image.filename).suffix}'
 
     # copy file to local disk
-    saveMe(image, target_file)
+    save_me(image, target_file)
 
     # send compression task to background
-    background_tasks.add_task(compressMe, target_file)
+    background_tasks.add_task(compress_me, target_file)
 
-    return {'url': '{0}/image/{1}{2}'.format(SERVER_HOST, pathlib.Path(target_file).stem, pathlib.Path(target_file).suffix)}
+    return {'url': f'{SERVER_HOST}/image/{pathlib.Path(target_file).stem}{pathlib.Path(target_file).suffix}'}
 
 
 @app.get('/image/{image_name}')
@@ -48,7 +50,7 @@ async def get_image(image_name: str):
     return FileResponse(f'{ORIGINAL_FOLDER}{image_name}')
 
 
-def saveMe(image: UploadFile, target_path: str):
+def save_me(image: UploadFile, target_path: str):
     #  open file for write
     with open(target_path, 'wb') as buffer:
         shutil.copyfileobj(image.file, buffer)
@@ -58,13 +60,13 @@ def saveMe(image: UploadFile, target_path: str):
     return
 
 
-def compressMe(file: str):
+def compress_me(file: str):
     # create folder if not exists
     if not os.path.exists(COMPRESS_FOLDER):
         os.makedirs(COMPRESS_FOLDER)
 
     # compress images
     picture = Image.open(file)
-    picture.save(COMPRESS_FOLDER + str(pathlib.Path(file).stem) + str(pathlib.Path(file).suffix), optimize=True, quality=10)
+    picture.save(f'{COMPRESS_FOLDER}{pathlib.Path(file).stem}{pathlib.Path(file).suffix}', optimize=True, quality=10)
 
     return
